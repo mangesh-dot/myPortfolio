39253ee5d57ebc4bbc0f15bc45c133dcfc21a51b
mangesh <kmangesh931@gmail.com>
2025-04-07 14:46:58 +0530

    added conf

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 10000,
+    allowedHosts: ['myportfolio-1-0p5h.onrender.com'],
   },
   preview: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 10000,
+    allowedHosts: ['myportfolio-1-0p5h.onrender.com'],
   }
 })
 
